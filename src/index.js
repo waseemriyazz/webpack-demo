d516a8e27b39a0c48a4258e46218b3b43d2d0358
waseemriyazz <waseemriyaz717@gmail.com>
2023-09-10 18:35:11 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+function component() {
+    const element = document.createElement('div');
+  
+   // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+  // import your function
+import myName from './myName';
+
+function component2() {
+  const element = document.createElement('div');
+
+  // use your function!
+  element.textContent = myName('Cody');
+  return element;
+}
+
+document.body.appendChild(component2());
+  
